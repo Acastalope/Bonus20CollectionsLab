11e64e827ea74422f5f8b760789a8249050337c1
Acastalope <acastalope@gmail.com>
2018-10-21 21:10:30 -0400

    second commit

--- src/Bonus20CollectionsLab.java
+++ src/Bonus20CollectionsLab.java
@@ -4,15 +4,8 @@ import java.util.Scanner;
 public class Bonus20CollectionsLab {
 
 	public static void main(String[] args) {
-
-		System.out.println("Welcome to Angie's Shop-n-Go!");
-		System.out.println();
-
-		System.out.printf("%2s %4s %10s", "Item#", "Item", "Price");
-		System.out.println();
-		System.out.printf("%2s %6s %9s", "====", "=====", "=====");
-		System.out.println();
-
+		Scanner sc = new Scanner(System.in);
+		
 		ArrayList<Integer> listItemNum = new ArrayList<Integer>();
 		listItemNum.add(1);
 		listItemNum.add(2);
@@ -49,18 +42,25 @@ public class Bonus20CollectionsLab {
 		listPrice.add(1.99);
 		listPrice.add(7.99);
 
-		for (int i = 0; i < listFood.size(); i++) {
-			System.out.println(listItemNum.get(i) + ". " + listFood.get(i) + " \t$" + listPrice.get(i));
-		}
+		System.out.println("Welcome to Angie's Shop-n-Go!");
+		System.out.println();
+
 
 	//	System.out.println("\nWhich item would you like to purchase? (enter Item# of food selection)");
 
-		Scanner sc = new Scanner(System.in);
+		
 //		sc.next();
 		
-		String cont = ("y");
+		String cont = "y";
 		
 		while (cont.equalsIgnoreCase("y")) {
+			System.out.printf("%2s %4s %10s", "Item#", "Item", "Price");
+			System.out.println();
+			System.out.printf("%2s %6s %9s", "====", "=====", "=====");
+			System.out.println();
+			for (int i = 0; i < listFood.size(); i++) {
+				System.out.println(listItemNum.get(i) + ". " + listFood.get(i) + " \t$" + listPrice.get(i));
+			}
 			System.out.println();
 			System.out.println("What item number would you like to purchase? ");
 			int userSelect = sc.nextInt();
@@ -68,18 +68,31 @@ public class Bonus20CollectionsLab {
 			System.out.println();
 			System.out.println(listFood.get((userSelect - 1)) + " added to cart at $" + listPrice.get((userSelect - 1)) + ".");
 			System.out.println("\nWould you like to continue shopping? y or n");
-			cont = sc.nextLine();
-		sc.next();
-	if (cont.equalsIgnoreCase("y")) {
-		for (int i = 0; i < listFood.size(); i++) {
-			System.out.println(listItemNum.get(i) + ". " + listFood.get(i) + " \t$" + listPrice.get(i));
+			//cont = sc.nextLine();
+			cont = sc.next();
 		}
-	}
-	else {
-			System.out.println("\nThanks for your order!");
-		}
-		System.out.println("\nSorry, we don't have that item. Please make another selection.");
+		System.out.println("\nThanks for your order!");
+
+		//sc.next();
+//	if (cont.equalsIgnoreCase("y")) {
+//		for (int i = 0; i < listFood.size(); i++) {
+//			
+//			System.out.println("Welcome to Angie's Shop-n-Go!");
+//			System.out.println();
+//
+//			System.out.printf("%2s %4s %10s", "Item#", "Item", "Price");
+//			System.out.println();
+//			System.out.printf("%2s %6s %9s", "====", "=====", "=====");
+//			System.out.println();
+//			
+//			System.out.println(listItemNum.get(i) + ". " + listFood.get(i) + " \t$" + listPrice.get(i));
+//		}
+//	}
+//	else {
+//			System.out.println("\nThanks for your order!");
+//		}
+//		System.out.println("\nSorry, we don't have that item. Please make another selection.");
 		
 		}
 }
-}
+
